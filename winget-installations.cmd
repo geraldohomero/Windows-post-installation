4a2581984a992b4f181cf8534e1ce5e910fc0d9f
Geraldo Homero <geraldohomero@protonmail.com>
2023-05-30 19:47:20 -0300

    Update winget-installations.cmd

--- winget-installations.cmd
+++ winget-installations.cmd
@@ -1,1 +1,3 @@
-winget install Microsoft.VisualStudio.2022.Community; 
+iwr -useb https://christitus.com/win | iex
+winget install Microsoft.VisualStudio.2022.Community; winget install 7zip.7zip; winget install Git.Git; winget install GitHub.GitHubDesktop; winget install Brave.Brave; winget install KDE.Okular; winget install LibreWolf.LibreWolf
+choco install vscode; choco install sql-server-express
