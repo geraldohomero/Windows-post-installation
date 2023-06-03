3327167dd76be3cdb0c9c08374e998fe46bb6449
Geraldo Homero <geraldohomero@protonmail.com>
2023-06-03 10:44:31 -0300

    Update winget-installations.ps1

--- winget-installations.ps1
+++ winget-installations.ps1
@@ -18,9 +18,8 @@ else {
 }
 
 # Install programs
-
+## Choco
 choco install vscode; sql-server-express
 
-
+## Winget 
 winget install Microsoft.VisualStudio.2022.Community; winget install 7zip.7zip; winget install Git.Git; winget install GitHub.GitHubDesktop; winget install Brave.Brave; winget install KDE.Okular; winget install LibreWolf.LibreWolf
-choco install vscode; choco install sql-server-express
