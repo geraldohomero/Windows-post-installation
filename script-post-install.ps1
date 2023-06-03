b80e5aa8f63fad2d377b84cd70009ae3ce0117e0
Geraldo Homero <geraldohomero@protonmail.com>
2023-06-03 11:13:55 -0300

    Update script-post-install.ps1

--- script-post-install.ps1
+++ script-post-install.ps1
@@ -23,3 +23,22 @@ choco install vscode; sql-server-express
 
 ## Winget 
 winget install Microsoft.VisualStudio.2022.Community; winget install 7zip.7zip; winget install Git.Git; winget install GitHub.GitHubDesktop; winget install Brave.Brave; winget install KDE.Okular; winget install LibreWolf.LibreWolf
+
+# Create folder Github
+$desktopPath = [Environment]::GetFolderPath("Desktop")
+$folderName = "Github"
+
+$folderPath = Join-Path $desktopPath $folderName
+
+# Verification
+if (!(Test-Path $folderPath)) {
+    # Create
+    New-Item -ItemType Directory -Path $folderPath | Out-Null
+    Write-Host "A pasta '$folderName' foi criada com sucesso na área de trabalho."
+}
+else {
+    Write-Host "A pasta '$folderName' já existe na área de trabalho."
+}
+
+Write-Host "Pressione qualquer tecla para fechar..."
+$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
