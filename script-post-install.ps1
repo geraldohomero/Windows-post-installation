2d57822e951c8e01b9e3f02dfe9a23e800fe2b96
Geraldo Homero <geraldohomero@protonmail.com>
2023-08-12 22:00:17 -0300

    Update script-post-install.ps1

--- script-post-install.ps1
+++ script-post-install.ps1
@@ -18,10 +18,8 @@ else {
 }
 
 # Install programs
-## Choco
 choco install vscode; sql-server-express
 
-## Winget  install
 winget install Microsoft.VisualStudio.2022.Community; winget install 7zip.7zip; winget install Git.Git; winget install GitHub.GitHubDesktop; winget install Brave.Brave; winget install KDE.Okular; winget install LibreWolf.LibreWolf
 
 # Create folder Github
@@ -38,7 +36,6 @@ if (!(Test-Path $folderPath)) {
 else {
     Write-Host "A pasta '$folderName' já existe na área de trabalho."
 }
-
 
 Write-Host "Pressione qualquer tecla para fechar..."
 $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
